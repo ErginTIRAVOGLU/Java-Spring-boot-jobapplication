51da3f24c61c087d43ee88b529465e06b0e92c02
Ergin <ergintiravoglu@msn.com>
2026-07-10 16:33:20 +0300

    Added Job entity class, service, repository and controller

---
+++ src/main/java/com/tiravoglu/quickstart/job/impl/JobServiceImpl.java
@@ -0,0 +1,68 @@
+package com.tiravoglu.quickstart.job.impl;
+
+import com.tiravoglu.quickstart.job.Job;
+import com.tiravoglu.quickstart.job.JobRepository;
+import com.tiravoglu.quickstart.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    //private List<Job> jobs = new ArrayList<>();
+    JobRepository jobRepository;
+    //private Long nextId= 1L;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+    @Override
+    public List<Job> findAll() {
+        return jobRepository.findAll();
+    }
+
+    @Override
+    public void createJob(Job job) {
+        //job.setId(nextId++);
+        jobRepository.save(job);
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+        return jobRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        try{
+            jobRepository.deleteById(id);
+            return true;
+        }
+        catch (Exception ex) {
+            return false;
+        }
+
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updatedJob) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+        if(jobOptional.isPresent()){
+            var job= jobOptional.get();
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            job.setMinSalary(updatedJob.getMinSalary());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setLocation(updatedJob.getLocation());
+            jobRepository.save(job);
+            return true;
+        }
+        return false;
+
+    }
+}
