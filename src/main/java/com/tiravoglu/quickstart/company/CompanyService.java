fe36f5c8c10fcd367dfce12353c12c7ba3f1e541
Ergin <ergintiravoglu@msn.com>
2026-07-11 12:01:33 +0300

    Added Company entity class, service, repository and controller

---
+++ src/main/java/com/tiravoglu/quickstart/company/CompanyService.java
@@ -0,0 +1,14 @@
+package com.tiravoglu.quickstart.company;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface CompanyService {
+    List<Company> getAllCompanies();
+    boolean updateCompany(Company company, Long id);
+    void createCompany(Company company);
+    boolean deleteCompanyById(Long id);
+    Company getCompanyById(Long id);
+}
--- src/main/java/com/tiravoglu/quickstart/job/Job.java
+++ src/main/java/com/tiravoglu/quickstart/job/Job.java
@@ -1,5 +1,6 @@
 package com.tiravoglu.quickstart.job;
 
+import com.tiravoglu.quickstart.company.Company;
 import jakarta.persistence.*;
 
 @Entity
@@ -13,6 +14,17 @@ public class Job {
     private String minSalary;
     private String maxSalary;
     private String location;
+
+    @ManyToOne
+    private Company company;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 
     public Job(Long id, String title, String description, String minSalary, String maxSalary, String location) {
         this.id = id;
